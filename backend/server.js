b418a9d17c5040487906b7663ba985f222a3e602
mitra-sanaathana <mitra.sanaathana@gmail.com>
2026-05-12 16:16:55 +0530

    fourth branch

--- backend/server.js
+++ backend/server.js
@@ -84,12 +84,10 @@ app.use((err, req, res, next) => {
 // Server
 // ======================
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log(
-    `Server running on port ${PORT}`
-  );
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on ${PORT}`);
 });
 
 // Serve static frontend
